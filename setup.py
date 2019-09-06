b4fdc050ed849058de3edceb00c1eaef819d94da
Keith Byrne <kmjbyrne@amazon.com>
2019-09-06 20:12:43 +0100

    build fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='kbpc',
-    version='0.1.4',
+    version='0.1.5',
     description='Reusable Python code for projects',
     long_description=long_description,
     long_description_content_type="text/markdown",
