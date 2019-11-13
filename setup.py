84b421a7052bd0f4690398f97e66751655f89e1a
Keith Byrne <keithmbyrne@gnmail.com>
2019-11-13 01:18:04 +0000

    migrated code from BNS API - version tagged

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Flask-KBPC',
-    version='0.4.2',
+    version='0.4.3',
     description='Reusable Python code for projects',
     long_description=long_description,
     long_description_content_type="text/markdown",
